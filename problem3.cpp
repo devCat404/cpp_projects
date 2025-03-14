25d8181ed0d350aa93784a8f4ad8d89b425c9650
devCat404 <kmh022460@naver.com>
2025-03-14 20:29:36 +0900

    chapter1 problem code upload

---
+++ problem3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void problem3() {
+    int tot = 0;
+
+    for (int i = 1; i <= 10; i++) {
+        tot += i;
+    }
+
+    cout << "1에서 10까지의 합은 " << tot << "입니다.\n\n";
+}
